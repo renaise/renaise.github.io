81eae47ce2b826d6bb1fea389099c828aaeb9016
Renaise <renaise@example.com>
2026-01-27 21:42:36 -0500

    Add custom domain CNAME, remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://renaise.github.io',
-  base: '/renaise-portfolio',
 });
